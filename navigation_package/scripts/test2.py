21549801901ebe1335b714db0ff7feb1fcf8715e
francimala <francesco.malacarne@gmail.com>
2019-05-11 18:32:55 +0200

    Project documentation introduced

--- navigation_package/scripts/test2.py
+++ navigation_package/scripts/test2.py
@@ -9,8 +9,14 @@ global position_tb3_0_x
 global position_tb3_0_y
 global position_tb3_1_x
 global position_tb3_1_y
+global goal_publisher0
+global goal_publisher1
+
+goal_publisher0 = rospy.Publisher("tb3_0/move_base_simple/goal", PoseStamped, queue_size=5)
+goal_publisher1 = rospy.Publisher("tb3_1/move_base_simple/goal", PoseStamped, queue_size=5)
 
 def callback0(msg):
+  #print("Executing callback0")
 
   global position_tb3_0_x
   global position_tb3_0_y
@@ -19,9 +25,9 @@ def callback0(msg):
   quat_tb3_0 = msg.pose.pose.orientation
   position_tb3_0_x = position_tb3_0.x
   position_tb3_0_y = position_tb3_0.y
-  #print("Executing callback0")
 
 def callback1(msg):
+  #print("Executing callback1")
 
   global position_tb3_1_x
   global position_tb3_1_y
@@ -30,7 +36,6 @@ def callback1(msg):
   quat_tb3_1 = msg.pose.pose.orientation
   position_tb3_1_x = position_tb3_1.x
   position_tb3_1_y = position_tb3_1.y
-  #print("Executing callback1")
  
 def callback(goal):
   #print("Executing callback")
@@ -39,6 +44,8 @@ def callback(goal):
   global position_tb3_0_y
   global position_tb3_1_x
   global position_tb3_1_y
+  global goal_publisher0
+  global goal_publisher1
 
   goal_position_x = goal.pose.position.x
   goal_position_y = goal.pose.position.y
@@ -58,13 +65,11 @@ def callback(goal):
   print(total_distance_tb3_1)
   
   if (total_distance_tb3_1>total_distance_tb3_0):
-    goal_publisher = rospy.Publisher("tb3_0/move_base_simple/goal", PoseStamped, queue_size=5)
-    goal_publisher.publish(goal)
+    goal_publisher0.publish(goal)
     print("CHECK 1")
 
   elif (total_distance_tb3_1<total_distance_tb3_0):
-    goal_publisher = rospy.Publisher("tb3_1/move_base_simple/goal", PoseStamped, queue_size=5)
-    goal_publisher.publish(goal)
+    goal_publisher1.publish(goal)
     print("CHECK 2")
 
 if __name__ == "__main__":
